f5d347705a7ccd6e5bb426b5fa912a78b10c8122
Francois de Kock <francois.dekock85@gmail.com>
2022-03-02 20:18:08 +0200

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -13,17 +13,15 @@ weatherForm.addEventListener("submit", (e) => {
   message1.textContent = "Loading...";
   message2.textContent = "";
 
-  fetch("http://localhost:3000/weather?address=" + location).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          message2.textContent = "";
-          message1.textContent = data.error;
-        } else {
-          message1.textContent = location;
-          message2.textContent = data.forecast;
-        }
-      });
-    }
-  );
+  fetch("/weather?address=" + location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        message2.textContent = "";
+        message1.textContent = data.error;
+      } else {
+        message1.textContent = location;
+        message2.textContent = data.forecast;
+      }
+    });
+  });
 });
